41822d6ab308e7686328989d4e92319084efcad5
dajun928 <1663177102@qq.com>
2019-08-27 00:23:48 +0800

    add by mac xwh

--- test.py
+++ test.py
@@ -7,13 +7,12 @@
 @func : 
 """
 import platform
-
 print(platform.python_version())
-
 print("add from mac")
 print("add from github")
 print("add from mac02")
 print("add from github")
 print("add from mac branch")
 print("add from win wdj")
+print("add from mac xwh")
 print("add from mac xwh")
